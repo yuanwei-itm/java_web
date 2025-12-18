b584e2f036bedf61903f214cec86fda87a3c8fab
yuanwei-itm <422258872@qq.com>
2025-12-18 15:21:18 +0800

    feat(pojo): 添加部门和统一返回结果实体类
    - 新增 Dept 类，包含部门的基本属性和时间戳
    - 新增 Result 类，用于统一接口返回格式
    - 删除旧的 BasicController 和 PathVariableController 控制器
    - 移除原有的 User 实体类定义

---
+++ tlias_management/src/main/java/com/yuanwei/pojo/Dept.java
@@ -0,0 +1,18 @@
+package com.yuanwei.pojo;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dept {
+
+    private Integer id;
+    private String name;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+}
---
+++ tlias_management/src/main/java/com/yuanwei/pojo/Result.java
@@ -0,0 +1,37 @@
+package com.yuanwei.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 统一返回结果
+ */
+@Data
+public class Result {
+    private Integer code;//状态码:1成功，0失败
+    private String msg;//提示信息
+    private Object data;// 数据
+
+    public static Result success(){
+    	Result result = new Result();
+    	result.code = 1;
+    	result.setMsg("操作成功");
+    	return result;
+    }
+
+    public static Result success(Object object){
+    	Result result = new Result();
+        result.data = object;
+    	result.code = 1;
+    	result.msg ="success";
+    	return result;
+    }
+
+    public static Result error(String msg){
+    	Result result = new Result();
+    	result.msg = msg;
+        result.code = 0;
+    	return result;
+    }
+}
