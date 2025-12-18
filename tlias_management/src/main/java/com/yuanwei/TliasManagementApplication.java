a22d0a6e68a0eb3e89c45100002ed6514c453672
yuanwei-itm <422258872@qq.com>
2025-12-18 14:35:37 +0800

    初始化文件

---
+++ tlias_management/src/main/java/com/yuanwei/TliasManagementApplication.java
@@ -0,0 +1,13 @@
+package com.yuanwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliasManagementApplication.class, args);
+    }
+
+}
---
+++ tlias_management/src/test/java/com/yuanwei/TliasManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanwei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TliasManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
