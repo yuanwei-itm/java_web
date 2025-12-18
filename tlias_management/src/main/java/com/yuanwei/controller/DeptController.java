4fa215c691a1ff89fb783f45e9ae4f0cd0364de9
yuanwei-itm <422258872@qq.com>
2025-12-18 16:11:41 +0800

    feat(dept): 更新部门控制器以支持GET请求并配置数据库连接
    - 将@RequestMapping注解替换为@GetMapping以明确指定HTTP方法
    - 添加RequestMethod导入以增强代码可读性
    - 配置application.yml文件以包含MySQL数据库连接信息
    - 设置MyBatis日志实现以便于调试SQL语句
    - 定义Spring Boot应用名称为spring-boot-tlias-management

--- tlias_management/src/main/java/com/yuanwei/controller/DeptController.java
+++ tlias_management/src/main/java/com/yuanwei/controller/DeptController.java
@@ -5,7 +5,9 @@ import com.yuanwei.pojo.Dept;
 import com.yuanwei.pojo.Result;
 import com.yuanwei.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +18,7 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
 
-    @RequestMapping("/depts")
+    @GetMapping("/depts")
     public Result list() {
         System.out.println("查询全部部门数据");
         List<Dept> deptList = deptService.findAll();
